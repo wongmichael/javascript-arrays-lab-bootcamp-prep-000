7d48f4a34a132bd425c5ed0e80ebd62adb282c2d
Michael Wong <mik9080@gmail.com>
2017-09-26 21:03:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(n){
+  kittens.pop()
+}
