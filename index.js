37ba6f22897a8d116a7da9dc417fde41b27f4325
Michael Wong <mik9080@gmail.com>
2017-09-26 21:05:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ function destructivelyAppendKitten(name){
 function destructivelyPrependKitten(name){
   kittens.unshift(name)
 }
-function destructivelyRemoveLastKitten(n){
+function destructivelyRemoveLastKitten(){
   kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+function appendKitten(n){
+  return [kittens,...n]
+}
+function prependKitten(n){
+  return [...n,kittens]
+}
+function removeLastKitten(){
+  return kittens.slice(0,kittens.length-1)
+}
+function removeFirstKitten(){
+  return kittens.slice(1)
 }
